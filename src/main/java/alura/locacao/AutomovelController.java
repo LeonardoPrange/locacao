6032c9d4d6049a7d291946da3770b954675e22ba
LeonardoPrange <leonardoprange@gmail.com>
2022-07-16 16:17:11 -0300

    implementa controllers com conexão direta ao banco de dados

--- src/main/java/alura/locacao/AutomovelController.java
+++ src/main/java/alura/locacao/AutomovelController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -14,23 +15,51 @@ import java.util.stream.Collectors;
 public class AutomovelController {
     private List<Automovel> automoveis = new ArrayList<Automovel>();
     @GetMapping
-    public ResponseEntity<ArrayList<AutomovelViewModel>> obtemAutomoveis() {
-        List<AutomovelViewModel> automoveisViewModel = this.automoveis.stream().map(_automovel -> {
-            AutomovelViewModel automovel = new AutomovelViewModel();
-            automovel.id = _automovel.getId();
-            automovel.marca = _automovel.getMarca();
-            automovel.modelo = _automovel.getModelo();
-            automovel.grupo = _automovel.getGrupo();
-            automovel.valorDiaria = _automovel.getValorDiaria();
-            automovel.quantidade = _automovel.getQuantidade();
-            return automovel;
-        }).collect(Collectors.toList());
-        ArrayList<AutomovelViewModel> automoveis = new ArrayList<AutomovelViewModel>(automoveisViewModel);
-        return new ResponseEntity(automoveis, HttpStatus.OK);
+    public ResponseEntity<ArrayList<AutomovelViewModel>> obtemAutomoveis() throws SQLException {
+        //Definição do comando SQL
+        String query = "SELECT * FROM public.automovel;";
+
+        //Lista para armazenar registros do banco de dados
+        ArrayList<AutomovelViewModel> automoveisViewModel = new ArrayList<AutomovelViewModel>();
+
+        //Obtem conexão com o banco de dados
+        try (Connection connection = JavaPostgreSql.obtemConexao();
+             //Formata comando
+             PreparedStatement pst = connection.prepareStatement(query);
+
+             //Executa comando
+             ResultSet rs = pst.executeQuery()) {
+
+            //Enquanto houver uma linha de resultado
+            while (rs.next()) {
+                //Obtem dados do banco e converte no formato do contrato da API
+                AutomovelViewModel automovel = new AutomovelViewModel();
+                automovel.id = rs.getObject("id", java.util.UUID.class);
+                automovel.marca = rs.getString("marca");
+                automovel.modelo = rs.getString("modelo");
+                automovel.grupo = Grupo.valueOf(rs.getString("grupo"));
+                automovel.valorDiaria = rs.getDouble("valor_diaria");
+                automovel.quantidade = rs.getInt("quantidade");
+                automoveisViewModel.add(automovel);
+            }
+        } catch (SQLException ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        //Retorna lista convertida
+        return new ResponseEntity(automoveisViewModel, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity cadastraAutomovel(@RequestBody CadastraAutomovelPayload automovelPayload) throws Exception {
+        //Definição do comando de inserção
+        String query = "INSERT INTO automovel(\n" +
+                "\tid, marca, modelo, grupo, valor_diaria, quantidade)\n" +
+                "\tVALUES (?, ?, ?, ?, ?, ?);";
+
+        //Obtem conexão com o banco de dados
+        Connection connection = JavaPostgreSql.obtemConexao();
+
+        //Instancia um novo automovel
         Automovel novoAutomovel = new Automovel(
                 automovelPayload.marca,
                 automovelPayload.modelo,
@@ -38,7 +67,18 @@ public class AutomovelController {
                 automovelPayload.valorDiaria,
                 automovelPayload.quantidade
         );
-        this.automoveis.add(novoAutomovel);
+
+        //Formata comando SQL com os valores dinamicos
+        PreparedStatement pst = connection.prepareStatement(query);
+        pst.setObject(1, novoAutomovel.getId(), Types.OTHER);
+        pst.setString(2, novoAutomovel.getMarca());
+        pst.setString(3, novoAutomovel.getModelo());
+        pst.setString(4, novoAutomovel.getGrupo().toString());
+        pst.setDouble(5, novoAutomovel.getValorDiaria());
+        pst.setInt(6, novoAutomovel.getQuantidade());
+
+        //Executa comando no banco de dados
+        pst.executeUpdate();
         return new ResponseEntity(HttpStatus.CREATED);
     }
 }
